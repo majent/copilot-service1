b3b10a0fcd0bd2bf51fac740fa793a3ec1118e2d
Ryo Omori <ryo763783@gmail.com>
2025-11-05 16:54:19 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
+function getGreeting() {
+  Let hour = new Date().getHours();
+  if (hour < 12) return "Good morning";
+  else if (hour < 18) return "Good afternoon";
+  else return "Good evening";
+}
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -13,9 +20,10 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h3>{getGreeting()}</h2>
+          <h2 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
-          </h1>
+          </h2>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
