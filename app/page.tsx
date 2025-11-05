f2692481599b91a3eb7f31b523760da9d3dedee5
Ryo Omori <ryo763783@gmail.com>
2025-11-05 19:26:40 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,12 @@ function getGreeting() {
   else return "Good evening";
 }
 
+function sum(array: number[]) {
+  return array.reduce(function (a, b) {
+    return a + b;
+  });
+}
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
